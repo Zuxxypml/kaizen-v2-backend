42699683799a68d5957a453e38363749a2d2df1e
Unknown <priscillasweetz952@gmail.com>
2023-01-12 17:24:47 +0100

    Edited Email Template

--- server.js
+++ server.js
@@ -11,8 +11,24 @@ const port = process.env.PORT || 5000;
 // ****** SEND API
 app.post("/send", async (req, res) => {
   try {
-    const { email, orderId, cartItems } = req.body;
-    await EmailSender({ email, orderId, cartItems });
+    const {
+      email,
+      orderId,
+      cartItems,
+      subTotal,
+      totalPrice,
+      shippingFee,
+      pickupLocation,
+    } = req.body;
+    await EmailSender({
+      email,
+      orderId,
+      cartItems,
+      subTotal,
+      totalPrice,
+      shippingFee,
+      pickupLocation,
+    });
     res.json({ msg: "Your message sent successfully" });
   } catch (error) {
     res.status(404).json({ msg: "Error ❌" });
